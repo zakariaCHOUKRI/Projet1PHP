483feeecbc1f63b842dc92ebe27e235411753fdc
Ayman Youss <youssayman4@gmail.com>
2023-03-06 09:06:46 +0100

    little advancement

--- index.php
+++ index.php
@@ -1,47 +1,74 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="Assets/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <title>Document</title>
-</head>
-<body>
-    <div class="landing">
-<button class="signup">Sign Up</button>
-<button class="login">Log in </button>
+<html>
+  <head>
+    <title>Login Page</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <div class="container mt-5">
+      <h1 class="text-center">Login</h1>
+      <form action="index.php" method="post" class="form-group">
+        <div class="form-group">
+          <label for="username">Username:</label>
+          <input type="text" id="username" name="username" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password" class="form-control">
+        </div>
+        <input type="submit" value="Submit" class="btn btn-primary btn-block">
+      </form> 
     </div>
-    <div class="loginForm dp-none">
-    <h1>Login</h1>
-<form method="GET" action = "index.php" >
-    <div class="i i1">
-    <label for="email">Email</label>
-<input type="email" name="email" id="email">
+    <div>
+        <?php
+        //$myAction = "";
+        $username = "root";
+        $password = "";
+        $hostname = "localhost";
+        //connection to the database
+        //$dbConnect = mysqli_connect($hostname, $username, $password);
+        // try to create connection
+        try {
+            $conn = new PDO("mysql:host=".$hostname.";dbname=employees", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+            echo "<br>";
+            }
+            // show an error if the connection was unsuccessful
+        catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+            }
+          
+          $queryTest = "SELECT * FROM employees LIMIT 5";
 
+          $res = $conn->query($queryTest);
+          
+          if (isset($_POST['username']) && isset($_POST['password']) && !empty($_POST['username']) && !empty($_POST['password'])){
+            echo "sdfsd";
+            $empUsername = $_POST['username'];
+            try{
+              $empNo = $conn->query("SELECT password FROM employees WHERE emp_no = $empUsername")->fetchAll(PDO::FETCH_ASSOC)[0]['password'];
+              $empBirth = $conn->query("SELECT birth_date FROM employees WHERE emp_no = $empUsername")->fetchAll(PDO::FETCH_ASSOC)[0]['birth_date'];
+              echo $empNo == $empBirth;
+            }
+            catch(PDOException $e){
+              echo "Unknown Username <br>";
+            }
+            
+            
+          }
+          // foreach($res->fetchAll(PDO::FETCH_ASSOC) as $row){
+          //   print_r($row);
+          //   echo "<br>";
+          // }
+          //print_r($res->fetchAll());
+          //echo $res->rowCount();
+    
+          
+    
+        ?>
     </div>
-    <div class="i i2">
-    <label for="password">Password</label>
-
-    <input type="password" name="password" id="password" required>
-    </div>
-<input type="submit" value="submit" class="submit">
-</form>
-        <button class="x">X</button>
-    </div>
-</body>
-<script>
-$(".login").on("click",function(){
-    $(".loginForm").css("display","block");
-    $(".landing").css("display","none");
-});
-$(".x").on("click",function(){
-    $(".loginForm").css("display","none");
-    $(".landing").css("display","flex");
-});
-</script>
+  </body>
 </html>
