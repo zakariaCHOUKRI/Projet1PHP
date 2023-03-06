aa99d0a547ba24848f0bf314dc090684c422bad8
anassKEMMOUNE <53840240+ari-xoz@users.noreply.github.com>
2023-03-06 18:57:09 +0100

    add admin file

---
+++ change_employee.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form  method="get">
+        <input type="text" name="search_emp" id="search_emp">
+    </form>
+    <?php 
+    try{
+        $conn = new PDO("mysql:host=".$hostname.";dbname=employees", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+     }
+     catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+        }
+        $querySearch = "SELECT"
+    ?>
+</body>
+</html>
